d07fb98bdfdde773d4ccd953719b1e332b6fd79e
E-moL0587 <kai.pika.025@outlook.com>
2024-10-19 18:00:29 +0900

    座標の読み込みと表示

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,31 @@
-export default function Home() {
+import { promises as fs } from 'fs';
+import path from 'path';
+
+type Coordinate = { x: number; y: number; z: number; };
+
+async function getCoordinates(): Promise<Coordinate[]> {
+  const filePath = path.join(process.cwd(), 'public', 'coordinates.json');
+  const jsonData = await fs.readFile(filePath, 'utf8');
+  const parsedData = JSON.parse(jsonData);
+
+  return parsedData.coordinates;
+}
+
+export default async function CoordinatesPage() {
+  const coordinates = await getCoordinates();
+
   return (
-    <div>
-      <h1>Hello world!</h1>
+    <div style={{ padding: "50px" }}>
+      <h3>座標</h3>
+
+      <p>
+        {coordinates.map((coord, index) => (
+          <span key={index}>
+            ({coord.x}, {coord.y}, {coord.z})
+            {index < coordinates.length - 1 && ", "}
+          </span>
+        ))}
+      </p>
     </div>
   );
 }
