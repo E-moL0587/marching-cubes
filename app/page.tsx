e0b99b477ef3f12d6211728a4a00dc29906d16bc
E-moL0587 <kai.pika.025@outlook.com>
2024-10-19 21:26:00 +0900

    コンテナ 追加

--- app/page.tsx
+++ app/page.tsx
@@ -22,12 +22,14 @@ export default function CoordinatesPage() {
     <div style={{ padding: "50px" }}>
       <h3>座標</h3>
 
-      {coordinates.slice(0, 3).map((coord, index) => (
-        <span key={index}>
-          ({coord.x}, {coord.y}, {coord.z})
-          {index < coordinates.length - 1 && ", "}
-        </span>
-      ))}
+      <div style={{ width: "300px", height: "100px", overflowY: "scroll", border: "1px solid #ccc", padding: "10px" }}>
+        {coordinates.map((coord, index) => (
+          <div key={index}>
+            ({coord.x}, {coord.y}, {coord.z})
+            {index < coordinates.length - 1 && ", "}
+          </div>
+        ))}
+      </div>
 
       <Canvas camera={{ position: [0, 0, 25] }} style={{ width: "300px", height: "300px", background: "#f0f0f0" }}>
         <OrbitControls />
